75d4cc0ffdef954741640f8e3aef1ab51c680649
Pascal <edouard.jp.pascal@gmail.com>
2016-09-06 11:15:07 +0200

    update files path

--- designate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "$1 Designate services "
-
-for DESIG_SERVICE in `ls /etc/init.d/designate-* | awk -F '/' ' { print $4;}'`; do
-        if  [ $DESIG_SERVICE != "designate-agent" ]; then
-                service $DESIG_SERVICE $1
-        fi
-done
